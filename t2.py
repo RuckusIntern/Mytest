d9d321aeea9503bfb4e3b7d9ab715b528ea21114
RuckusIntern <hsuan.yeh@commscope.com>
2022-06-24 11:03:17 +0800

    good

--- t2.py
+++ t2.py
@@ -1,22 +1,25 @@
 import pymysql
 
-db =pymysql.connect(
-    host='172.17.0.2',
-    user='test',
-    password='1',
-    database='testDB'
-    )
-
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
- 
-# 使用 execute()  方法执行 SQL 查询 
-cursor.execute("SELECT VERSION()")
- 
-# 使用 fetchone() 方法获取单条数据.
-data = cursor.fetchone()
- 
-print ("Database version : %s " % data)
- 
-# 关闭数据库连接
-db.close()
+# 資料庫設定
+db_settings = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "1",
+    "db": "test",
+    "charset": "utf8"
+}
+try:
+   # 建立Connection物件
+    conn = pymysql.connect(**db_settings)
+    # 建立Cursor物件
+    with conn.cursor() as cursor:
+         # 查詢資料SQL語法
+        command = "SELECT * FROM users"
+        # 執行指令
+        cursor.execute(command)
+        # 取得所有資料
+        result = cursor.fetchall()
+        print(result)
+except Exception as ex:
+    print(ex)
